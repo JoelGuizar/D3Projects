18a457d0eda49a79dc85e5abf4451d51bd8c9864
Joel Guizar <joelguizar@gmail.com>
2018-02-28 00:46:32 -0500

    server initialized

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
   console.log();
 });
 
